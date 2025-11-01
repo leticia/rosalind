3328904d30aaa7441daff25c23f3b1f500c4b5ed
Leticia Figueira <jlcfigueira@gmail.com>
2025-11-01 14:52:59 -0300

    Add a solution to DNA in Python

---
+++ DNA/DNA.py
@@ -0,0 +1,12 @@
+#/bin/env python3
+def count_nucleotides(dna):
+    a_count = dna.count('A')
+    c_count = dna.count('C')
+    g_count = dna.count('G')
+    t_count = dna.count('T')
+    return a_count, c_count, g_count, t_count
+
+if __name__ == "__main__":
+    sample_dna = open("rosalind_dna.txt").read().strip()
+    a, c, g, t = count_nucleotides(sample_dna)
+    print(a, c, g, t)
