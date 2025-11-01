5f08138da59ff3c3182f6423fea5dc2ae13f1206
Leticia Figueira <jlcfigueira@gmail.com>
2025-11-01 15:07:06 -0300

    Add a solution to REVC in Python

---
+++ REVC/REVC.py
@@ -0,0 +1,11 @@
+#/bin/env python3
+def reverse_complement(dna):
+    complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+    reversed_dna = dna[::-1]
+    reverse_complement_dna = ''.join(complement[base] for base in reversed_dna)
+    return reverse_complement_dna
+
+if __name__ == "__main__":
+    sample_dna = open("rosalind_revc.txt").read().strip()
+    rev_comp = reverse_complement(sample_dna)
+    print(rev_comp)
